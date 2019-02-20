3356a42c3abf79a625d3cdbf26eacaa49f973fb7
Gabe Velasquez <gvelasquez@trackforce.com>
2019-02-20 15:50:48 -0800

    added list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../Hero";
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: "app-heroes",
@@ -7,6 +8,7 @@ import { Hero } from "../Hero";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
+  hereos = HEROES;
   hero: Hero = {
     id: 1,
     name: "Windstorm"
